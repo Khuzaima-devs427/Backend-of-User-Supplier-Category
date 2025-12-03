20022080374e56610f29eccd183942071c53631b
Syed Khuzaima Gillani <khuzaimadevs@gmail.com>
2025-12-03 19:14:29 +0500

    new

--- src/app.ts
+++ src/app.ts
@@ -2,27 +2,23 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from 'cors';
 import connectDB from "./config/db";
-import  mainRouter  from './routes/index';
+import mainRouter from './routes/index';
 
-dotenv.config(); // load .env file
-connectDB(); // connect to MongoDB
-
-
-
+dotenv.config();
+connectDB();
 
 const app = express();
-app.use(express.json()); // for parsing JSON data
 
 app.use(cors({
   origin: ['http://localhost:3000'],
   credentials: true,
 }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
 app.use('/api', mainRouter);
-
 
 app.get("/", (req, res) => {
   res.send("API is running...");
@@ -30,3 +26,8 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
+
+
+
+
