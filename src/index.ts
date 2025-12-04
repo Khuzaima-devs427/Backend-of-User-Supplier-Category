18437481de368998b64ff5f8c4e437ff0818c86f
Syed Khuzaima Gillani <khuzaimadevs@gmail.com>
2025-12-04 19:03:22 +0500

    changes

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from 'cors';
+import connectDB from "./config/db";
+import mainRouter from './routes/index';
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+// Routes
+app.use('/api', mainRouter);
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
+
+
+
+
